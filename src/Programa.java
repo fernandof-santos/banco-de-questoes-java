45b035f560e05955a83cf9bad4db6355efdad278
Fernando Ferreira <ferreira.fe.08@gmail.com>
2026-08-17 16:13:46 -0300

    feat: adiciona cadastro de questões com alternativas

--- src/Programa.java
+++ src/Programa.java
@@ -6,6 +6,11 @@ public class Programa {
 
         Scanner sc = new Scanner (System.in);
         ArrayList<String> perguntas = new ArrayList<>();
+        ArrayList<String> alternativasA = new ArrayList<>();
+        ArrayList<String> alternativasB = new ArrayList<>();
+        ArrayList<String> alternativasC = new ArrayList<>();
+        ArrayList<String> alternativasD = new ArrayList<>();
+        ArrayList<String> respostasCorretas = new ArrayList<>();
 
        while (true) {
         System.out.println("==== BANCO DE QUESTÕES ====");
@@ -15,6 +20,7 @@ public class Programa {
         System.out.println("3 - Buscar Questão");
         System.out.println("4 - Atualizar Questão");
         System.out.println("5 - Remover Questão");
+        System.out.println("6 - Simulado");
         System.out.println("0 - Sair");
 
         System.out.println(" ");
@@ -32,16 +38,45 @@ public class Programa {
                    String pergunta = sc.nextLine();
                    perguntas.add(pergunta);
 
-                   System.out.print("Digite a resposta: ");
+                   System.out.print("Digite a alternativa A: ");
+                   String alternativaA = sc.nextLine();
+                   alternativasA.add(alternativaA);
 
-                   System.out.print("Digite a categoria: ");
+                   System.out.print("Digite a alternativa B: ");
+                   String alternativaB = sc.nextLine();
+                   alternativasB.add(alternativaB);
 
-                   System.out.println("Digite a dificuldade: ");
+                   System.out.print("Digite a alternativa C: ");
+                   String alternativaC = sc.nextLine();
+                   alternativasC.add(alternativaC);
+
+                   System.out.print("Digite a alternativa D: ");
+                   String alternativaD = sc.nextLine();
+                   alternativasD.add(alternativaD);
+
+                   System.out.print("Digite a resposta correta: ");
+                   String resposta = sc.nextLine().toUpperCase();
+
+
+                   while (! (resposta.equals("A") || resposta.equals("B") || resposta.equals("C") || resposta.equals("D"))) {
+                       System.out.println("Resposta Inválida!");
+                       System.out.println("Digite novamente: ");
+                       resposta = sc.nextLine().toUpperCase();
+                   }
+                   respostasCorretas.add(resposta);
+                   System.out.println(" ");
+                   System.out.println("Questão cadastrada com sucesso!");
+                   System.out.println (" ");
 
                    break;
 
                case 2:
-                   System.out.println("  ");
+                   System.out.println("===== QUESTÕES =====");
+                   System.out.println(" ");
+
+                   for (String questao: perguntas) {
+                       System.out.println(questao);
+                   }
                    break;
 
                case 3:
@@ -54,6 +89,10 @@ public class Programa {
 
                case 5:
                    System.out.println("     ");
+                   break;
+
+               case 6:
+                   System.out.println("      ");
                    break;
 
                case 0:
